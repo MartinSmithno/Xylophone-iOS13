111eb47b22040e9618fb2c8b044098084420afab
= <=>
2022-02-05 02:32:37 +0100

    Opacity Added

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -21,6 +21,14 @@ class ViewController: UIViewController {
         //Unlem isaretinin anlami "ben kontrol ettim sender.currentTitle her zaman String veriyor" demektir
         playSound(fileName: sender.currentTitle!)
         print(sender.currentTitle!)
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
     }
     
     
